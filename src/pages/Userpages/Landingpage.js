024959fd472c8f894f93328ddb7461ae709c90a5
hirunppal <hirunppal@gmail.com>
2022-06-24 00:25:26 +0700

    feat:colusel-landing-authmodal-itemcards

--- src/pages/Userpages/Landingpage.js
+++ src/pages/Userpages/Landingpage.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Carusel from '../../components/Client/layout/Carusel';
+import CardItems from '../../components/Client/layout/flashsale/CardItems';
 
 function Landingpage() {
   return (
     <>
       <Carusel />
+      <CardItems />
     </>
   );
 }
