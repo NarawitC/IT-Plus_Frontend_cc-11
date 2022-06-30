01337b9945a3e53eb13598b3233b6f21a9ae5e36
Panit24 <lgwamhmmams@gmail.com>
2022-06-30 17:32:09 +0700

    edit product detail page 3

--- src/pages/supplier/DynamicSelectedProductPage.js
+++ src/pages/supplier/DynamicSelectedProductPage.js
@@ -4,6 +4,7 @@ import subPicture2 from '../../productImg/item2.jpg';
 import subPicture3 from '../../productImg/item3.jpg';
 import subPicture4 from '../../productImg/item4.jpg';
 import { AiOutlineNumber } from 'react-icons/ai';
+
 function DynamicSelectedProductPage() {
   const mockObj = {
     stock: 2,
@@ -15,9 +16,13 @@ function DynamicSelectedProductPage() {
     brand: 'Edifier',
     category: 'computer-labtop',
     subCategory: 'จอคอม',
+    description:
+      'นอกจากไมค์คุณภาพสูงแล้วหูฟังเล่นเกม Logitech Pro X ยังมาพร้อมกับการปรับแต่งเสียงผ่าน Software ได้ในตัว ทำให้นอกจากใช้เล่นเกมแล้วยังสามารถเอาไปใช้ ดูหนังฟังเพลงได้อีกด้วย โดยการเซฟ Presets เอาไว้ อีกทั้งยังมี Sound Card มาช่วยดันคุณภาพเสียงให้สูงขึ้นได้อีกด้วย แถมบอดี้ของมันยังแข็งแรงด้วยวัสดุโลหะ และอลูมิเนียม ด้วยการผสมกันของวัสดุทั้งสองอย่างทำให้ได้หูฟังที่แข็งแรงและน้ำหนักเบาสุด ๆ ',
   };
   return (
-    <div>
+    <div className=''>
+      <br />
+      <br />
       <div className='flex justify-center'>
         <div className='flex flex-col'>
           <img
@@ -32,7 +37,7 @@ function DynamicSelectedProductPage() {
       </div>
       <br />
       <div className='flex gap-2 justify-center'>
-        <div className='flex flex-col'>
+        <div className='flex flex-col '>
           <img
             className='h-60 w-auto object-contain border-2 p-4 rounded-lg hover:border-primary'
             src={subPicture1}
@@ -70,74 +75,76 @@ function DynamicSelectedProductPage() {
         </div>
       </div>
       <br />
-      <div className='gap-2  grid  mb-6 lg:grid-cols-2'>
-        <div className=''>
-          <div className='flex flex-col justify-center'>
-            <label
-              htmlFor='productName'
-              className='block mb-2 text-sm font-bold text-gray-1200 '
-            >
-              ชื่อสินค้า
-            </label>
-            <p>{mockObj.productName}</p>
-          </div>
+      <div className='gap-4  grid  mb-6 lg:grid-cols-2 p-4'>
+        <div className='flex flex-col justify-center border-2 hover:border-info rounded-lg p-2'>
+          <label
+            htmlFor='productName'
+            className='block mb-2 text-sm font-bold text-gray-1200 '
+          >
+            ชื่อสินค้า
+          </label>
+          <p>{mockObj.productName}</p>
         </div>
-        <div className=''>
-          <div className='flex flex-col justify-center'>
-            <label
-              htmlFor='category'
-              className='block mb-2 text-sm font-bold text-gray-1200 '
-            >
-              หมวดหมู่สินค้า
-            </label>
-            <p>{mockObj.category}</p>
-          </div>
+        <div className='flex flex-col justify-center border-2 hover:border-info rounded-lg p-2'>
+          <label
+            htmlFor='category'
+            className='block mb-2 text-sm font-bold text-gray-1200 '
+          >
+            หมวดหมู่สินค้า
+          </label>
+          <p>{mockObj.category}</p>
         </div>
-        <div className=''>
-          <div className='flex flex-col justify-center'>
-            <label
-              htmlFor='subCategory'
-              className='font-bold block mb-2 text-sm  text-gray-1200 '
-            >
-              หมวดหมู่สินค้าย่อย
-            </label>
-            <p>{mockObj.subCategory}</p>
-          </div>
+        <div className='flex flex-col justify-center border-2 hover:border-info rounded-lg p-2'>
+          <label
+            htmlFor='subCategory'
+            className='font-bold block mb-2 text-sm  text-gray-1200 '
+          >
+            หมวดหมู่สินค้าย่อย
+          </label>
+          <p>{mockObj.subCategory}</p>
         </div>
-        <div className=''>
-          <div className='flex flex-col justify-center'>
-            <label
-              htmlFor='price'
-              className='block mb-2 text-sm font-bold text-gray-1200 '
-            >
-              ราคาสินค้า
-            </label>
-            <p>{mockObj.price}</p>
-          </div>
+
+        <div className='flex flex-col justify-center border-2 hover:border-info rounded-lg p-2'>
+          <label
+            htmlFor='price'
+            className='block mb-2 text-sm font-bold text-gray-1200 '
+          >
+            ราคาสินค้า
+          </label>
+          <p>{mockObj.price.toFixed(2)}</p>
         </div>
-        <div className=''>
-          <div className='flex flex-col justify-center'>
-            <label
-              htmlFor='brand'
-              className='block mb-2 text-sm font-bold text-gray-1200 '
-            >
-              แบรนด์สินค้า
-            </label>
-            <p>{mockObj.brand}</p>
-          </div>
+        <div className='flex flex-col justify-center border-2 hover:border-info rounded-lg p-2'>
+          <label
+            htmlFor='brand'
+            className='block mb-2 text-sm font-bold text-gray-1200 '
+          >
+            แบรนด์สินค้า
+          </label>
+          <p>{mockObj.brand}</p>
         </div>
-        <div className=''>
-          <div className='flex flex-col justify-center'>
-            <label
-              htmlFor='stock'
-              className='block mb-2 text-sm font-bold text-gray-1200 '
-            >
-              จำนวน
-            </label>
-            <p>{mockObj.stock}</p>
-          </div>
+        <div className='flex flex-col justify-center border-2 hover:border-info rounded-lg p-2'>
+          <label
+            htmlFor='stock'
+            className='block mb-2 text-sm font-bold text-gray-1200 '
+          >
+            Stock
+          </label>
+          <p>{mockObj.stock}</p>
         </div>
       </div>
+      <div className='flex flex-col justify-center border-2 hover:border-info rounded-lg m-4 p-2'>
+        <label
+          for='description'
+          className='block mb-2 text-md font-bold text-gray-1200 '
+        >
+          รายละเอียดสินค้า
+        </label>
+        <p>{mockObj.description}</p>
+        <br />
+        <br />
+      </div>
+      <br />
+      <br />
     </div>
   );
 }
