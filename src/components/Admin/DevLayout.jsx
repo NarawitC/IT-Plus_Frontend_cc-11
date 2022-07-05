a4452b671f435d3dee68936026f38518ff92f4ae
NarawitC <narawit.developer@outlook.com>
2022-07-05 20:56:28 +0700

    COMPLETED

--- src/components/Admin/DevLayout.jsx
+++ src/components/Admin/DevLayout.jsx
@@ -4,14 +4,18 @@ import DevSearchBar from './DevSearchBar';
 import DevSideBar from './DevSideBar';
 import { Outlet } from 'react-router-dom';
 import { checkLocation } from '../../services/checkLocation';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useParams } from 'react-router-dom';
 
 function DevLayout() {
   const location = useLocation();
-  const { isAdminPromotionPage } = checkLocation(location);
+  const { isAdminProductPage } = checkLocation(location);
+  const params = useParams();
+  const isAdminSelectedProductPage =
+    params.productId && isAdminProductPage ? true : false;
+  console.log(isAdminSelectedProductPage);
   return (
     <div data-theme='cupcake' className='w-full h-screen '>
-      <DevSearchBar />
+      {isAdminSelectedProductPage || <DevSearchBar />}
       <DevSideBar />
       <div>
         <Outlet />
