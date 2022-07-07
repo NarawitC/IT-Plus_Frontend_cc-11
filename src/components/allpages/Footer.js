e2b99f1ac77a1b1a968155b510a2a1f3c0a484d9
Aesidoz <aesido_gunz@hotmail.com>
2022-07-07 14:13:29 +0700

    feat:footer,googlemap add address

--- src/components/allpages/Footer.js
+++ src/components/allpages/Footer.js
@@ -1,7 +1,29 @@
 import React from 'react';
+import Logo from '../../../../IT-Plus_Frontend_cc-11/src/icons/LOGO1.svg';
+import { useNavigate } from 'react-router-dom';
 
 function Footer() {
-  return <div className='h-40 gradeient1'>footer</div>;
+  const navigate = useNavigate();
+  return (
+    <div className='  mt-20 h-40 bg-gradient-to-b from-blue-900 to-blue-800   text-font-Kanit  pt-6 '>
+      <div className=' flex justify-center  py-2'>
+        {/* <svg width='100' height='100'>
+        </svg> */}
+        {/* <Logo className=' w-64 ' /> */}
+        <img src={Logo} className='w-40 ' />
+      </div>
+      <div className='flex justify-center'>
+        <div
+          className='btn btn-primary flex justify-center w-40 rounded-3xl  bg-white text-primary border-none  hover:text-white '
+          onClick={() => {
+            navigate('/supplier');
+          }}
+        >
+          ร่วมเป็นผู้ขายกับเรา
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Footer;
