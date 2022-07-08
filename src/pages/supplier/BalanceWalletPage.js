67646f6d06f596f0350d9b72ded0835705fd9fb1
Panit24 <lgwamhmmams@gmail.com>
2022-07-08 12:03:18 +0700

    axios transaction 12:03

--- src/pages/supplier/BalanceWalletPage.js
+++ src/pages/supplier/BalanceWalletPage.js
@@ -1,8 +1,13 @@
 import { RiMoneyDollarBoxLine } from 'react-icons/ri';
 import { TiDocumentText } from 'react-icons/ti';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { BiDetail } from 'react-icons/bi';
+import {
+  getAllTransactionsBySupplierId,
+  createWithdrawalTransaction,
+} from '../../apis/supplier/supplierTransaction';
+import { getBalanceBySupplierId } from '../../apis/supplier/supplierBalance';
 function BalanceWalletPage() {
   const navigate = useNavigate();
 
@@ -11,48 +16,86 @@ function BalanceWalletPage() {
 
   const mockArr = [
     {
-      date: '2022-06-19',
+      createdAt: '2022-06-19',
       type: 'รับ',
       description: 'รายรับจาก order 200425EAN',
       amount: 100.0,
       status: 'COMPLETED',
+      Order: { id: 0 },
     },
     {
-      date: '2022-06-18',
+      createdAt: '2022-06-18',
       type: 'รับ',
       description: 'รายรับจาก order 200325EAN',
       amount: 900.0,
       status: 'COMPLETED',
+      Order: { id: 0 },
     },
     {
-      date: '2022-06-17',
+      createdAt: '2022-06-17',
       type: 'รับ',
       description: 'รายรับจาก order 200435EAN',
       amount: 1000.0,
       status: 'COMPLETED',
+      Order: { id: 0 },
     },
     {
-      date: '2022-06-16',
+      createdAt: '2022-06-16',
       type: 'รับ',
       description: 'รายรับจาก order 200335EAN',
       amount: 1000.0,
       status: 'COMPLETED',
+      Order: { id: 0 },
     },
     {
-      date: '2022-06-15',
+      createdAt: '2022-06-15',
       type: 'ถอน',
       description: 'การถอนไปยังเลขที่บัญชี (*1234)',
       amount: 2000.0,
-      status: 'IN PROGRESS',
+      status: 'PENDING',
+      Order: { id: 0 },
     },
     {
-      date: '2022-06-14',
+      createdAt: '2022-06-14',
       type: 'ถอน',
       description: 'การถอนไปยังเลขที่บัญชี (*1235)',
       amount: 1000.0,
       status: 'COMPLETED',
+      Order: { id: 0 },
     },
   ];
+  const [transactions, setTransactions] = useState([]);
+  const [mapTransactionDetails, setMapTransactionDetails] = useState([]);
+  const [balance, setBalance] = useState(0);
+  const [withdrawalAmount, setWithdrawalAmount] = useState('');
+
+  const handleGetAllTransactionsBySupplierId = async () => {
+    try {
+      const res = await getAllTransactionsBySupplierId();
+      setTransactions(res.data.transactions);
+      setMapTransactionDetails(res.data.transactions);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    handleGetAllTransactionsBySupplierId();
+  }, [mapTransactionDetails]);
+
+  const handleGetBalanceBySupplierId = async () => {
+    try {
+      const res = await getAllTransactionsBySupplierId();
+      setBalance(res.data.balance);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    handleGetBalanceBySupplierId();
+  }, [balance]);
+
   return (
     <>
       <div className='mt-16'>
@@ -60,7 +103,7 @@ function BalanceWalletPage() {
           <h1 className='text-4xl text-center font-bold'>Seller Balance</h1>
           <br />
           <p className='text-info text-center text-4xl font-bold'>
-            ฿ 24,000.00
+            {+balance.toFixed(2) || (0.0).toFixed(2)}
           </p>
         </div>
         <br />
@@ -136,23 +179,37 @@ function BalanceWalletPage() {
           <br />
           <div className='flex justify-between text-2xl'>
             <p>จำนวนเงินถอน</p>
-            <p>10000.00</p>
+            <input
+              type='text'
+              id='amount'
+              className=' text-end text-xl w-[135px] bg-yellow-100 border border-gray-300 text-gray-1200 font-bold rounded-lg   p-4 '
+              placeholder='จำนวนเงิน'
+              required
+              value={withdrawalAmount}
+              onChange={(event) => setWithdrawalAmount(event.target.value)}
+            />
           </div>
           <br />
           <div className='flex justify-between text-2xl'>
             <p>ค่าธรรมเนียม</p>
-            <p>0.00</p>
+            <p className='pr-3'>{(0.0).toFixed(0)}</p>
           </div>
           <br />
           <div className='flex justify-between text-2xl'>
             <p>รวม</p>
-            <p className='font-bold'>10000.00</p>
+            <p className='font-bold pr-3'>{+withdrawalAmount || 0.0}</p>
           </div>
         </div>
         <br />
         <br />
         <div className='flex justify-center'>
-          <button className='btn btn-secondary w-[140px] hover:scale-105 font-bold text-2xl'>
+          <button
+            type='button'
+            className='btn btn-secondary w-[140px] hover:scale-105 font-bold text-2xl'
+            onClick={() => {
+              createWithdrawalTransaction(withdrawalAmount);
+            }}
+          >
             ยืนยัน
           </button>
         </div>
@@ -173,12 +230,18 @@ function BalanceWalletPage() {
             </tr>
           </thead>
           <tbody>
-            {mockArr.map((el, idx) => {
+            {mapTransactionDetails?.map((el, idx) => {
               return (
                 <>
                   <tr className='text-center hover' key={idx}>
                     <td className='text-center font-bold'>{idx + 1}</td>
-                    <td className='font-bold'>{el.date}</td>
+                    <td className='font-bold'>{`${el?.createdAt.slice(
+                      8,
+                      10
+                    )}${el?.createdAt.slice(4, 7)}-${el?.createdAt.slice(
+                      0,
+                      4
+                    )}`}</td>
                     {el.type === 'รับ' ? (
                       <>
                         <td className='text-success font-bold'>{el.type}</td>
@@ -192,9 +255,10 @@ function BalanceWalletPage() {
                       {el.type === 'รับ' ? (
                         <>
                           <button
+                            type='buttom'
                             className='  font-bold text-ghost hover:text-blue-900 hover:scale-105'
                             onClick={() => {
-                              navigate('/supplier/order/selected');
+                              navigate(`/supplier/order/${el?.Order?.id}`);
                             }}
                           >
                             {el.description}
@@ -222,9 +286,10 @@ function BalanceWalletPage() {
                     )}
                     <td>
                       <button
+                        type='button'
                         className='flex w-[70px] justify-center hover:scale-125 text-info font-bold '
                         onClick={() => {
-                          navigate('/supplier/transaction/selected');
+                          navigate(`/supplier/transaction/${el?.id}`);
                         }}
                       >
                         {<BiDetail size={25} />}
